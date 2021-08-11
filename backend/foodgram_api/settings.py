6cbadd3248dd9841d8ffc410b2078ecc494fbbeb
Александр Медведев <xelam11@yandex.ru>
2021-08-11 15:00:53 +0300

    add tag, ingredient, recipe models

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'users',
+    'recipes',
 ]
 
 MIDDLEWARE = [
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
