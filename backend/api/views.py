7e284e953f502e4cba744bd141eaa34f7e0f6102
Александр Медведев <xelam11@yandex.ru>
2021-08-17 13:11:02 +0300

    correct PEP8 ver.1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
