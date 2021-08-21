b12adcb4c0ac8eda4e68ac4f060cd30dbee78759
Александр Медведев <xelam11@yandex.ru>
2021-08-21 12:59:42 +0300

    correct deploy ver.23

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -94,11 +94,11 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/backend_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/backend_media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "backend_media")
 
 
 REST_FRAMEWORK = {
