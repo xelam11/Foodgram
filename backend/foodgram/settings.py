b88221b35d4229504e2c1454d7bb545a61194d02
Александр Медведев <xelam11@yandex.ru>
2021-08-21 10:33:56 +0300

    correct deploy ver.19

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,9 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rg-0(_o_nwg=cgzlq_@t7d4%9-()yp=ufjvq=c=*v2(ybwhf^3'
 
 DEBUG = True
-ALLOWED_HOSTS = ['*',
-                 '178.154.254.55',
-                 'backend']
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
